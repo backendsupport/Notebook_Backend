3d839d7a9151fc52534fbb094cb3ea29818216bb
dhruv patel <dhruv160916@gmail.com>
2022-12-21 11:46:20 +0530

    make changes

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const user=process.env.DB_USER;
 const pass=process.env.DB_PASSWORD;
 const name=process.env.DB_NAME;
 const connectToMongo=()=>{
-    mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0.nsa5j.mongodb.net/${name}?retryWrites=true&w=majority`).then(()=>{
+    mongoose.connect(`mongodb+srv://dhruvpdpu:dhRuv16o9@cluster0.3zczu.mongodb.net/${name}?retryWrites=true&w=majority`).then(()=>{
     }).catch((error)=>{
     console.log(error);
     });
