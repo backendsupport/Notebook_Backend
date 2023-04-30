17880138b9050303b0e76f6e47d96d6a95b1446c
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-04-30 14:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 const port = 5000;
 var cors=require('cors');
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 //Availabe Notes
 app.use('/api/auth',require('./routes/auth'))
