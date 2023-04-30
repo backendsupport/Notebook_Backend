7d9814f81209b795a3acc593d1cd4561f19254d8
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-04-30 13:51:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const connectToMongo=require('./db');
-connectToMongo();
+const connectToMongo = require('./db');
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const port = 5000;
-var cors=require('cors');
+const port = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectToMongo();
+
+// Enable CORS
 app.use(cors());
+
+// Parse JSON bodies
 app.use(express.json());
-//Availabe Notes
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+// Default route
 app.get('/', (req, res) => {
-  res.send(`There is Nothing here`)
-})
-app.listen(process.env.PORT || port, () => {
-  console.log(`iNotebook_Backend listening at http://localhost:${port}`)
-})
+  res.send(`There is Nothing here`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`iNotebook_Backend listening at http://localhost:${port}`);
+});
